d742def6023fdc9e378b67661efbf663ef3b3501
Kateryna <tsymbal.katena@mail.ru>
2023-01-03 17:22:19 -0600

    singlr player

--- blue.py
+++ blue.py
@@ -13,3 +13,31 @@ def print_tic_tac_toe(value):
     print("\t  {}  |  {}  |  {}".format(values[6], values[7], values[8]))
     print("\t     |     |")
     print("\n")
+
+def single_game(cur_player):
+
+    # Represents the Tic Tac Toe
+    values = [' ' for x in range(9)]
+
+    # Stores the positions occupied by X and O
+    player_pos = {'X':[], 'O':[]}
+
+    while True:
+    print_tic_tac_toe(values)
+
+    try:
+        print("Player ", cur_player, " turn. Which box? : ", end="")
+        move = int(input())
+    except ValueError:
+        print("Wrong Input!!! Try Again")
+        continue
+
+    # Sanity check for MOVE inout
+    if move < 1 or move > 9:
+        print("Wrong Input!!! Try Again")
+        continue
+
+    # Check if the box is not occupied already
+    if values[move-1] != ' ':
+        print("Place already filled. Try again!!")
+        continue
